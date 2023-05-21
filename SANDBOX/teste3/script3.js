93d96ab7aed92bf17fda1a5baf8366497aa04f52
Rafael Santos <rlsantos@gmail.com>
2023-05-21 19:40:13 -0300

    mostrar kernel

--- SANDBOX/teste3/script3.js
+++ SANDBOX/teste3/script3.js
@@ -11,9 +11,21 @@ function handleFiles() {
 	console.log(this.files[0]);
 	image.load(window.URL.createObjectURL(this.files[0]), imageLoaded);
 }
+////////////////
 
+var kernel = 
+	[-1, 0, 1,
+	-1, 0, 1,
+	-1,0, 1];
 
-/////////////////////////////////////////////////
+	//Tenho que normalizar o Kernel para preservar brilho da imagem
+	//Normalizando Kernel (soma dos elementos = 1)
+	//Se a soma dos elementos é 0, retorno
+	let soma = kernel.reduce((b, c) => b + c) == 0 ? 1 : kernel.reduce((b, c) => b + c)
+	kernel = kernel.map(a => (a / soma))
+	let dim = Math.sqrt(kernel.length);
+
+/////////////////
 image = new RafImage();
 image.load("kitten3.jpg", imageLoaded);
 console.log(image)
@@ -26,18 +38,7 @@ function imageLoaded() {
 	canvas1.height = image.height;
 	image.draw(canvas1)
 
-	var kernel = 
-	[-1, 0, 1,
-	-1, 0, 1,
-	-1,0, 1];
 
-	//Tenho que normalizar o Kernel para preservar brilho da imagem
-	//Normalizando Kernel (soma dos elementos = 1)
-	//Se a soma dos elementos é 0, retorno
-	let soma = kernel.reduce((b, c) => b + c) == 0 ? 1 : kernel.reduce((b, c) => b + c)
-	kernel = kernel.map(a => (a / soma))
-	console.log(kernel)
-	let dim = Math.sqrt(kernel.length);
 	const pad = Math.floor(dim / 2);
 
 	const pixels = image.imageData.data;
@@ -77,6 +78,30 @@ function imageLoaded() {
 	imageOut.draw(canvas2)
 	//imageOut.draw(canvas2)
 }
+
+
+/////////////////////////////////////////////////
+///Tabela com o kernel
+
+let table = document.getElementById("tabela");
+table.setAttribute('border', '1');
+var tbl = "<tbody>"
+
+for ( let ky = 0 ; ky < Math.sqrt(kernel.length) ; ky ++){
+	tbl += "<tr>"  //criar nova linha
+	console.log(tbl)
+		for (let kx = 0 ; kx < Math.sqrt(kernel.length); kx++){
+			tbl += "<td>" +  kernel[ky*dim + kx] + "</td>" //cada celula da linha
+	}
+	tbl += "</tr>" //fecha cada linha
+}
+tbl += "</tbody>"
+table.innerHTML = tbl
+
+
+
+
+
 
 
 
