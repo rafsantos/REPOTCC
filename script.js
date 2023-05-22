19c6f170458798b477c9e1f70379bf285def6c68
Rafael Santos <rlsantos@gmail.com>
2023-05-22 01:42:33 -0300

    Kernel personalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var canvas1 = document.getElementById("canvas1");
 var canvas2 = document.getElementById("canvas2");
-
+if (kernel == null)
+	var kernel = new Kernel();
 
 //////////////////////////////////////////////////
 
@@ -11,26 +12,51 @@ function handleFiles() {
 	console.log(this.files[0]);
 	image.load(window.URL.createObjectURL(this.files[0]), imageLoaded);
 }
-////////////////
 
-var kernel = 
-	[-1, 0, 1,
-	-1, 0, 1,
-	-1,0, 1];
 
+/////////////////////////
+// Objeto Kernel. construtor, normalização, etc.
+function Kernel(ker) {
+	if (Array.isArray(ker)) {
+		if (Math.sqrt(ker.length) % 1 === 0) {
+			this.kernel = ker;
+		}
+	}
+	if (ker == null) {
+		this.kernel =
+			[-1, 0, 1,
+			-1, 0, 1,
+			-1, 0, 1];
+	}
 	//Tenho que normalizar o Kernel para preservar brilho da imagem
 	//Normalizando Kernel (soma dos elementos = 1)
 	//Se a soma dos elementos é 0, retorno
-	let soma = kernel.reduce((b, c) => b + c) == 0 ? 1 : kernel.reduce((b, c) => b + c)
-	kernel = kernel.map(a => (a / soma))
-	let dim = Math.sqrt(kernel.length);
+	this.soma = this.kernel.reduce((b, c) => b + c) == 0 ? 1 : this.kernel.reduce((b, c) => b + c);
+	this.kernel = this.kernel.map(a => (a / this.soma));
+	this.length = this.kernel.length;
+	this.dim = Math.sqrt(this.kernel.length);
+	updateTable(this); //Atualiza visualização em tela
+}
+
+//////////////////////////////////////////
 
 /////////////////
 image = new RafImage();
 image.load("gato.jpg", imageLoaded);
 console.log(image)
 
+
+
 function imageLoaded() {
+	processar();
+}
+
+
+function processar() {
+
+	var ker = kernel.kernel;
+	var dim = kernel.dim;
+
 	console.log(image)
 	imageOut = image.clone()
 
@@ -40,8 +66,6 @@ function imageLoaded() {
 
 
 	const pad = Math.floor(dim / 2);
-
-	const pixels = image.imageData.data;
 	const width = image.width;
 	const height = image.height;
 
@@ -63,9 +87,9 @@ function imageLoaded() {
 
 					i = (ky + pad) * dim + (kx + pad); //kernel
 
-					r += image.getR(x + kx, y + ky) * kernel[i];
-					g += image.getG(x + kx, y + ky) * kernel[i];
-					b += image.getB(x + kx, y + ky) * kernel[i];
+					r += image.getR(x + kx, y + ky) * ker[i];
+					g += image.getG(x + kx, y + ky) * ker[i];
+					b += image.getB(x + kx, y + ky) * ker[i];
 
 				}
 			}
@@ -82,26 +106,34 @@ function imageLoaded() {
 
 /////////////////////////////////////////////////
 ///Tabela com o kernel
-
-let table = document.getElementById("tabela");
-table.setAttribute('border', '1');
-var tbl = "<tbody>"
-
-for ( let ky = 0 ; ky < Math.sqrt(kernel.length) ; ky ++){
-	tbl += "<tr>"  //criar nova linha
-	console.log(tbl)
-		for (let kx = 0 ; kx < Math.sqrt(kernel.length); kx++){
-			tbl += "<td>" +  kernel[ky*dim + kx] + "</td>" //cada celula da linha
+function updateTable(kernel) {
+	let table = document.getElementById("tabela");
+	table.setAttribute('border', '1');
+	var tbl = "<tbody>"
+	for (let ky = 0; ky < Math.sqrt(kernel.length); ky++) {
+		tbl += "<tr>"  //criar nova linha
+		console.log(tbl)
+		for (let kx = 0; kx < Math.sqrt(kernel.length); kx++) {
+			tbl += "<td>" + kernel.kernel[ky * kernel.dim + kx] + "</td>" //cada celula da linha
+		}
+		tbl += "</tr>" //fecha cada linha
 	}
-	tbl += "</tr>" //fecha cada linha
+	tbl += "</tbody>"
+	table.innerHTML = tbl
 }
-tbl += "</tbody>"
-table.innerHTML = tbl
 
+/////////////////////////////////
+// Lendo os valores para um Kernel novo de entrada
+function formIn() {
+	console.log("form enviado");
+	let kernVals = document.getElementsByName("kernelIn");
+	var newKernel = []
+	for (kernVal of kernVals) {
+		console.log(kernVal.value);
+		(isNaN(kernVal.value) || !kernVal.value) ? newKernel.push(0) : newKernel.push(parseInt(kernVal.value));
+	}
+	console.log(newKernel);
+	kernel = new Kernel(newKernel);
 
-
-
-
-
-
+}
 
